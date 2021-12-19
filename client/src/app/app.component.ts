35f4f7b052f17899b817afd7881a59abee0443c9
18127057NTA <85469538+18127057NTA@users.noreply.github.com>
2021-12-19 10:10:27 +0700

    End of Section 8
    Interface for Product, Pagination on AngularJS

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+//Khi chạy trang chủ - OnInit
+export class AppComponent implements OnInit {
+  //Tên thương hiệu
   title = 'Marketnet';
+
+  //products: any[]; // Trước đó
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  //Lấy danh sách sản phẩm,pageSize tối đa là 20 mà để dôi ra 50
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      //(response: any) => { // trước đó
+      (response: IPagination) => {
+        this.products = response.data;
+        //console.log(response); //trước đó
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
---
+++ client/src/app/models/product.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  productType: string;
+  productTypeId: number;
+  store: string;
+  storeId: number;
+  storeAddress: string;
+  supplierId: number;
+  supplierName: string;
+  quantity: number;
+  unitPrice: number;
+  pictureUrl: string;
+}
