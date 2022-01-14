28bb9a80ed41e71946dd0966a8744e3a6ca83346
18127057NTA <85469538+18127057NTA@users.noreply.github.com>
2022-01-14 16:16:18 +0700

    End of Section 20
    Orders History

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -45,10 +45,17 @@ const routes: Routes = [
     data: { breadcrumb: 'Checkout' },
   },
   {
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./orders/orders.module').then((mod) => mod.OrdersModule),
+    data: { breadcrumb: 'Đơn hàng' },
+  },
+  {
     path: 'account',
     loadChildren: () =>
       import('./account/account.module').then((mod) => mod.AccountModule),
-    data: { breadcrumb: {skip: true} },
+    data: { breadcrumb: { skip: true } },
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
